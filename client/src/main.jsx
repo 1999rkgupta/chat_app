34ea735ac77f45eb9e7b779055ba08cdba5dc76f
1999rkgupta <1999rkgupta@gmail.com>
2024-02-12 13:39:49 +0530

    context provider added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { AuthContextProvider } from "./Context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Toaster position="top-center" reverseOrder={false} />
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </Router>
   </React.StrictMode>
 );
