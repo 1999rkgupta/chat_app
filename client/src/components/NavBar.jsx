251b5321160df8cdc753ac5405b5b1696fb3103b
1999rkgupta <1999rkgupta@gmail.com>
2024-02-12 13:40:05 +0530

    conditional rendering added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { Container, Navbar, Nav, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
 
 const NavBar = () => {
+  const { user, logoutUser } = useContext(AuthContext);
   return (
     <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
       <Container>
@@ -10,15 +13,36 @@ const NavBar = () => {
             Chat App
           </Link>
         </h2>
-        <span className="text-warning">Logged in as Ranjan</span>
+        {user && (
+          <span className="text-warning">Logged in as {user?.name}</span>
+        )}
+
         <Nav>
           <Stack direction="horizontal" gap="3">
-            <Link to="/login" className="link-light text-decoration-none">
-              Login
-            </Link>
-            <Link to="/register" className="link-light text-decoration-none">
-              Register
-            </Link>
+            {user && (
+              <>
+                <Link
+                  to="/login"
+                  onClick={() => logoutUser()}
+                  className="link-light text-decoration-none"
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </Stack>
         </Nav>
       </Container>
