888a0885f1066ba4afe7f06d9c1c01f17ecd1c62
1999rkgupta <1999rkgupta@gmail.com>
2024-02-12 13:40:30 +0530

    register logic added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,17 @@
+import { useContext } from "react";
 import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { AuthContext } from "../Context/AuthContext";
 const Register = () => {
+  const {
+    registerInfo,
+    updateRegisterInfo,
+    RegisterUser,
+    registerError,
+    registerLoading,
+  } = useContext(AuthContext);
   return (
     <>
-      <Form>
+      <Form onSubmit={RegisterUser}>
         <Row
           style={{
             height: "100vh",
@@ -13,15 +22,44 @@ const Register = () => {
           <Col xs={6}>
             <Stack gap="3">
               <h2>Register</h2>
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="Email" />
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                value={registerInfo.name}
+                onChange={e => {
+                  updateRegisterInfo({ ...registerInfo, name: e.target.value });
+                }}
+              />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                value={registerInfo.email}
+                onChange={e => {
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    email: e.target.value,
+                  });
+                }}
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={registerInfo.password}
+                onChange={e => {
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  });
+                }}
+              />
               <Button variant="primary" type="submit">
-                Register
+                {registerLoading ? "Creating User" : "Register"}
               </Button>
-              <Alert variant="danger">
-                <p>An error occured</p>
-              </Alert>
+              {registerError?.error && (
+                <Alert variant="danger">
+                  <p>{registerError?.message}</p>
+                </Alert>
+              )}
             </Stack>
           </Col>
         </Row>
